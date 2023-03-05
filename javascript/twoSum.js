5dc0caa21040fff942500b96c0b45888970c46a5
Josué Hoenicka <josuee.vzla@gmail.com>
2023-03-05 20:22:08 -0300

    create: add twoSum.js

---
+++ javascript/twoSum.js
@@ -0,0 +1,19 @@
+// You can see the challenge prompt at the following link below:
+// https://leetcode.com/problems/two-sum/
+
+// My answer
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const otherNum = target - num;
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] === otherNum) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+};
+
+// Test
+console.log(twoSum([2,7,11,15], 9)); // [0,1]
